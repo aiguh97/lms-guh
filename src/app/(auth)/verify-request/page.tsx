9e78c712d9d4825261c8a5386be494e4519ec255
minguh <aiguh97@gmail.com>
2025-11-02 19:23:45 +0700

    teguh

--- src/app/(auth)/verify-request/page.tsx
+++ src/app/(auth)/verify-request/page.tsx
@@ -1,7 +1,14 @@
+// src/app/(auth)/verify-request/page.tsx
 "use client";
 export const dynamic = "force-dynamic";
 
-import VerifyRequestComponent from "./VerifyRequestComponent";
+import nextDynamic from "next/dynamic";
+
+// Load component sepenuhnya CSR
+const VerifyRequestComponent = nextDynamic(
+  () => import("./VerifyRequestComponent"),
+  { ssr: false } // disable SSR
+);
 
 export default function Page() {
   return <VerifyRequestComponent />;
